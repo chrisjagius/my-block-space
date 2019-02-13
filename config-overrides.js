595364bcb25fe4954ee12716ae3e4fc129cfecba
Chance Baars <chancebaars@gmail.com>
2019-02-12 21:14:04 -0500

    config webpack with react-app-rewire-blockstack

--- config-overrides.js
+++ config-overrides.js
@@ -1,88 +1,23 @@
-const path = require('path');
-const { paths } = require('react-app-rewired');
-const StatsPlugin = require('stats-webpack-plugin');
+const {
+  rewireBlockstackBuild,
+  rewireBlockstackDevServer
+} = require('react-app-rewire-blockstack')
 
 module.exports = {
-  webpack: function (config, env) {
-    if (env === 'production') {
-      if (!config.plugins) {
-        // It should already exist, but initialize it if it does not already exist.
-        config.plugins = [
-          HtmlWebpackPluginConfig,
-          ManifestAssetPlugin,
-          IconAssetPlugin,
-          new UglifyJsPlugin({
-            uglifyOptions: {
-              mangle: {
-                reserved: [
-                  'Buffer',
-                  'BigInteger',
-                  'Point',
-                  'ECPubKey',
-                  'ECKey',
-                  'sha512_asm',
-                  'asm',
-                  'ECPair',
-                  'HDNode'
-                ]
-              }
-            }
-          })
-        ];
-      }
-      config.plugins.push(
-        new StatsPlugin('stats.json', {
-          // Replace the next 2 lines with any options you need to give to the stats plugin - these are their example options.
-          chunkModules: true,
-          exclude: [/node_modules[\\\/]react/]
-        })
-      );
-      config.module.rules[1].oneOf[1].include = [
-        paths.appSrc,
-        path.resolve(paths.appNodeModules, 'bitcoinjs-lib'),
-        path.resolve(paths.appNodeModules, 'tiny-secp256k1'),
-        path.resolve(paths.appNodeModules, 'jsontokens'),
-        path.resolve(paths.appNodeModules, 'bip32'),
-        path.resolve(paths.appNodeModules, 'base64url'),
-        path.resolve(paths.appNodeModules, 'typeforce'),
-        path.resolve(paths.appNodeModules, 'key-encoder'),
-        path.resolve(paths.appNodeModules, 'base-x'),
-        path.resolve(paths.appNodeModules, 'uri-js')
-      ];
-    }
-    return config;
-  },
-  jest: function (config) {
-    return config;
-  },
-  // configFunction is the original react-scripts function that creates the
-  // Webpack Dev Server config based on the settings for proxy/allowedHost.
-  // react-scripts injects this into your function (so you can use it to
-  // create the standard config to start from), and needs to receive back a
-  // function that takes the same arguments as the original react-scripts
-  // function so that it can be used as a replacement for the original one.
-  devServer: function (configFunction) {
-    return function(proxy, allowedHost) {
-      const config = configFunction(proxy, allowedHost);
-      // Edit config here - example: set your own certificates.
-      //
-      // const fs = require('fs');
-      // config.https = {
-      //   key: fs.readFileSync(process.env.REACT_HTTPS_KEY, 'utf8'),
-      //   cert: fs.readFileSync(process.env.REACT_HTTPS_CERT, 'utf8'),
-      //   ca: fs.readFileSync(process.env.REACT_HTTPS_CA, 'utf8'),
-      //   passphrase: process.env.REACT_HTTPS_PASS
-      // };
-      config.historyApiFallback = {
-        disableDotRule: true
-      },
-			config.headers = {
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-			}
 
-      return config;
-    };
+  webpack: (config, env) => {
+    if (env === 'production') {
+      config = rewireBlockstackBuild(config)
+    }
+    return config
+  },
+
+  devServer: (configFunction) => {
+    return (proxy, allowedHost) => {
+      let config = configFunction(proxy, allowedHost)
+      config = rewireBlockstackDevServer(config)
+      return config
+    }
   }
+
 }
