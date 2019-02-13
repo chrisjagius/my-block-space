e5c7b0da8e62f4cf419d81c543526a7eeb5a0e6c
Chance Baars <chancebaars@gmail.com>
2019-02-12 20:26:57 -0500

    config webpack

--- config-overrides.js
+++ config-overrides.js
@@ -4,18 +4,19 @@ const { paths } = require('react-app-rewired');
 module.exports = {
   webpack: function (config, env) {
     if (env === 'production') {
-      config.module.rules[1].oneOf[1].include = [
-        paths.appSrc,
-        path.resolve(paths.appNodeModules, 'bitcoinjs-lib'),
-        path.resolve(paths.appNodeModules, 'tiny-secp256k1'),
-        path.resolve(paths.appNodeModules, 'jsontokens'),
-        path.resolve(paths.appNodeModules, 'bip32'),
-        path.resolve(paths.appNodeModules, 'base64url'),
-        path.resolve(paths.appNodeModules, 'typeforce'),
-        path.resolve(paths.appNodeModules, 'key-encoder'),
-        path.resolve(paths.appNodeModules, 'base-x'),
-        path.resolve(paths.appNodeModules, 'uri-js')
-      ];
+      config.module.rules[1].oneOf[1] = {
+        include: [
+          paths.appSrc,
+          path.resolve(paths.appNodeModules, 'bitcoinjs-lib'),
+          path.resolve(paths.appNodeModules, 'tiny-secp256k1'),
+          path.resolve(paths.appNodeModules, 'jsontokens'),
+          path.resolve(paths.appNodeModules, 'bip32')
+        ],
+        loader: require.resolve('babel-loader'),
+        options: {
+          compact: true
+        }
+      }
     }
     return config;
   },
