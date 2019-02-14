5def6c0b63c93f498bfec86f280ff8695a04d472
Chance Baars <chancebaars@gmail.com>
2019-02-14 15:28:45 -0500

    added babel@7
    Signed-off-by: Chance Baars <chancebaars@gmail.com>

--- config-overrides.js
+++ config-overrides.js
@@ -1,75 +1,35 @@
 const path = require('path');
 const { paths } = require('react-app-rewired');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ManifestAssetPlugin = new CopyWebpackPlugin([{ from: 'public/manifest.json', to: 'manifest.json' }]);
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
 
 module.exports = {
   webpack: function (config, env) {
-    if (env === 'production') {
-      config.module.rules[1].oneOf[1].include = [
-        paths.appSrc,
-        path.resolve(paths.appNodeModules, 'bitcoinjs-lib'),
-        path.resolve(paths.appNodeModules, 'tiny-secp256k1'),
-        path.resolve(paths.appNodeModules, 'jsontokens'),
-        path.resolve(paths.appNodeModules, 'bip32'),
-        path.resolve(paths.appNodeModules, 'base64url'),
-        path.resolve(paths.appNodeModules, 'typeforce'),
-        path.resolve(paths.appNodeModules, 'key-encoder'),
-        path.resolve(paths.appNodeModules, 'base-x'),
-        path.resolve(paths.appNodeModules, 'uri-js')
-      ];
-      // config.plugins = [HtmlWebpackPluginConfig,
-      //   ManifestAssetPlugin,
-      //   new UglifyJsPlugin({
-      //     uglifyOptions: {
-      //       mangle: {
-      //         reserved: [
-      //           'Buffer',
-      //           'BigInteger',
-      //           'Point',
-      //           'ECPubKey',
-      //           'ECKey',
-      //           'sha512_asm',
-      //           'asm',
-      //           'ECPair',
-      //           'HDNode'
-      //         ]
-      //       }
-      //     }
-      //   })];
-      config.loader = {loader: require.resolve('babel-loader')}
-      
+
+    config.module.rules[1].oneOf[1].test= /\.(js|jsx|mjs)$/,
+    config.module.rules[1].oneOf[1].include = [
+      paths.appSrc,
+      path.resolve(paths.appNodeModules, 'bitcoinjs-lib'),
+      path.resolve(paths.appNodeModules, 'tiny-secp256k1'),
+      path.resolve(paths.appNodeModules, 'jsontokens'),
+      path.resolve(paths.appNodeModules, 'bip32'),
+      path.resolve(paths.appNodeModules, 'base64url'),
+      path.resolve(paths.appNodeModules, 'typeforce'),
+      path.resolve(paths.appNodeModules, 'key-encoder'),
+      path.resolve(paths.appNodeModules, 'base-x'),
+      path.resolve(paths.appNodeModules, 'uri-js')
+    ];
+
+    config.module.rules[1].oneOf[1].loader= require.resolve('babel-loader')
+    config.module.rules[1].oneOf[1].options= {
+      compact: true
     }
     return config;
   },
   jest: function (config) {
     return config;
   },
-  // configFunction is the original react-scripts function that creates the
-  // Webpack Dev Server config based on the settings for proxy/allowedHost.
-  // react-scripts injects this into your function (so you can use it to
-  // create the standard config to start from), and needs to receive back a
-  // function that takes the same arguments as the original react-scripts
-  // function so that it can be used as a replacement for the original one.
   devServer: function (configFunction) {
     return function(proxy, allowedHost) {
       const config = configFunction(proxy, allowedHost);
-      // Edit config here - example: set your own certificates.
-      //
-      // const fs = require('fs');
-      // config.https = {
-      //   key: fs.readFileSync(process.env.REACT_HTTPS_KEY, 'utf8'),
-      //   cert: fs.readFileSync(process.env.REACT_HTTPS_CERT, 'utf8'),
-      //   ca: fs.readFileSync(process.env.REACT_HTTPS_CA, 'utf8'),
-      //   passphrase: process.env.REACT_HTTPS_PASS
-      // };
       config.historyApiFallback = {
         disableDotRule: true
       },
