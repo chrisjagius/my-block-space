1223219477d29b4765ae4fbc0f7e4d32c1a424ef
Chance Baars <chancebaars@gmail.com>
2019-02-15 10:41:41 -0500

    added yarn build

---
+++ build/precache-manifest.12a9eb1f5441373d12d8665c540413e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "937fec69c5ab180a7602f0aaf364435a",
+    "url": "/static/media/users.937fec69.svg"
+  },
+  {
+    "revision": "e65a62485a3a8f9761aebe99e9115df1",
+    "url": "/static/media/standard-wallpaper.e65a6248.jpg"
+  },
+  {
+    "revision": "674d9d35ab7539fdb20307c4867a13b0",
+    "url": "/static/media/settings.674d9d35.svg"
+  },
+  {
+    "revision": "8b7b6457eb083cb8e519e63d7067c68e",
+    "url": "/static/media/camera.8b7b6457.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56bcda0fddc21431b241",
+    "url": "/static/js/main.56bcda0f.chunk.js"
+  },
+  {
+    "revision": "910c4d91ef393238464f",
+    "url": "/static/js/1.910c4d91.chunk.js"
+  },
+  {
+    "revision": "56bcda0fddc21431b241",
+    "url": "/static/css/main.c77e3ec7.chunk.css"
+  },
+  {
+    "revision": "e93ee7d35b33908648a6065078cad8cb",
+    "url": "/index.html"
+  }
+];
