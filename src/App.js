15f2c35cf2fee4a43470459ee8b28ad2ea3f89bc
Chance Baars <chancebaars@gmail.com>
2019-03-21 18:06:12 -0400

    reload page after signin

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
       return true;
     } else if (isSignInPending()) {
       handlePendingSignIn().then(function (userData) {
+        window.location.reload();
       })
       return false;
     }
